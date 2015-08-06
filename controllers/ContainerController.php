615b595f323f3e2e5983340ec7ff90d391a504a5
Will Wharton <willmwharton@gmail.com>
2015-08-05 20:49:19 -0700

    container view

--- controllers/ContainerController.php
+++ controllers/ContainerController.php
@@ -29,10 +29,55 @@ class ContainerController extends BaseDockerController
 
     public function actionView($id)
     {
-        $container = \Yii::$app->getModule('docker')->Containers()->find($id);
-
+        $container  =   \Yii::$app->getModule('docker')->Containers()->find($id);
+        $config     =   $container->getConfig();
+        $info       =   $container->getRuntimeInformations();
         return $this->render('view',[
-            'container'  => $container,
+            'container' =>  $container,
+            'config'    =>  $config,
+            'info'      =>  $info,
+            'envDP'     =>  $this->getEnvDataProvider($info),
+            'volDP'     =>  $this->getVolDataProvider($info),
         ]);
+    }
+
+    public function getEnvDataProvider($info)
+    {
+        $env = array();
+        foreach($info['Config']['Env'] as $k=>$v)
+        {
+            $p = explode('=', $v, 2);
+            $env[] = [
+                'id'    =>  $k,
+                'var'   =>  $p[0],
+                'value' =>  $p[1]
+            ];
+        }
+        $envDP = new ArrayDataProvider([
+            'allModels' =>  $env,
+        ]);
+
+        return $envDP;
+    }
+
+
+    public function getVolDataProvider($info)
+    {
+        $vol = array();
+        $volId = 0;
+        foreach($info['Volumes'] as $k => $v )
+        {
+            $vol[] = [
+                'id'        =>  $volId++,
+                'local'     =>  $k,
+                'remote'    =>  $v,
+                'rw'        =>  (array_key_exists($k,$info['VolumesRW']) && $info['VolumesRW'][$k] == 1)?'rw':'ro',
+            ];
+        }
+        $volDP = new ArrayDataProvider([
+            'allModels' =>  $vol,
+        ]);
+
+        return $volDP;
     }
 }
--- views/container/view.php
+++ views/container/view.php
@@ -3,23 +3,115 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\GridView;
+use yii\widgets\DetailView;
 
 $this->title = 'View';
 $this->params['breadcrumbs'][] = ['label' => 'Containers', 'url' => ['/docker/container']];
 $this->params['breadcrumbs'][] = $this->title;
 
-?>
-<div class="docker-container-index">
 
+
+?>
+<div class="row">
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-heading">Container Config</div>
+            <div class="panel-body">
+                <?php
+                echo DetailView::widget([
+                    'model' => $info,
+                    'attributes' => [
+                        [
+                            'attribute' =>  'Config.Image',
+                            'label'  => 'Image',
+                        ],
+                        [
+                            'attribute' =>  'Config.Cmd',
+                            'label'  => 'Command',
+                            'type'  =>  'raw',
+                            'value' => join($info['Config']['Cmd'],' ')
+                        ],
+                        [
+                            'attribute' =>  'Config.Hostname',
+                            'label'  => 'Hostname',
+                        ],
+                        [
+                            'attribute' =>  'Config.ExposedPorts',
+                            'label'  => 'Ports',
+                            'type'  =>  'raw',
+                            'value' => join(array_keys($info['Config']['ExposedPorts']),' ')
+                        ],
+                        'Created',
+                    ],
+                ]);
+
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-heading">Enviroment Variables</div>
+            <div class="panel-body">
+                <?php
+                    echo GridView::widget([
+                        'layout'        =>  '{items}',
+                        'showHeader'    =>  false,
+                        'dataProvider'  =>  $envDP,
+                        'columns'       =>  [
+                            'var',
+                            'value',
+                        ]
+                    ]);
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="row">
+    <div class="col-md-12">
+
+        <div class="panel">
+            <div class="panel-heading">Volumes</div>
+            <div class="panel-body">
+                <?php
+
+                    echo GridView::widget([
+                        'layout'        =>  '{items}',
+                        'showHeader'    =>  false,
+                        'dataProvider'  =>  $volDP,
+                        'columns'       =>  [
+                            'rw',
+                            'local',
+                            'remote',
+                        ]
+                    ]);
+
+
+                ?>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+<div class="row">
+    <div class="col-md-12">
     <div class="panel panel-docker-container">
         <div class="panel-heading">Container View</div>
         <div class="panel-body">
             <div class="table-responsive">
-                <pre><?php print_r($container->getConfig()); ?></pre>
-                <pre><?php print_r($container->getRuntimeInformations()); ?></pre>
+                <pre><?php print_r($config); ?></pre>
+                <pre><?php print_r($info); ?></pre>
             </div>
         </div>
     </div>
-
-
+    </div>
 </div>
